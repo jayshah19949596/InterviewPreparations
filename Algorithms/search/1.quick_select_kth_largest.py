73db88895dc9fab570e688e78c7a7a065a61b93c
Jai Shah <jaishah@amazon.com>
2023-12-25 11:59:24 -0800

    Update varaible names to use locally scoped variables.

--- Algorithms/search/1.quick_select_kth_largest.py
+++ Algorithms/search/1.quick_select_kth_largest.py
@@ -1,30 +1,30 @@
-def partition(arr, low, high):
+def partition(array, low, high):
     i = low - 1  # index of smaller element
     pivot_idx = high
     pivot_element = array[pivot_idx]  # pivot element
     for j in range(low, high):
-        if arr[j] <= pivot_element:
+        if array[j] <= pivot_element:
             i += 1
-            arr[i], arr[j] = arr[j], arr[i]
+            array[i], array[j] = arr[j], array[i]
     array[i + 1], array[pivot_idx] = array[pivot_idx], array[i + 1]
     return i + 1
 
-def quickselect(arr, low, high, k):
+def quickselect(array, low, high, k):
     if low <= high:
-        pivot_index = partition(arr, low, high)
+        pivot_index = partition(array, low, high)
 
         if pivot_index == k:
-            return arr[pivot_index]
+            return array[pivot_index]
         elif pivot_index < k:
-            return quickselect(arr, pivot_index + 1, high, k)
+            return quickselect(array, pivot_index + 1, high, k)
         else:
-            return quickselect(arr, low, pivot_index - 1, k)
+            return quickselect(array, low, pivot_index - 1, k)
 
-def find_kth_largest(arr, k):
-    n = len(arr)
+def find_kth_largest(array, k):
+    n = len(array)
     if k > 0 and k <= n:
         # kth largest element is at index n - k
-        return quickselect(arr, 0, n - 1, n - k)
+        return quickselect(array, 0, n - 1, n - k)
     else:
         return "Invalid value of k"
 
